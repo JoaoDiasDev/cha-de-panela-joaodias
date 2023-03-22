8755e0874f0fc7824801d1fb0a05867d842d16e0
JoaoDias <joaodiasworking@gmail.com>
2023-03-21 22:45:36 -0300

    Sending images from products

--- weddingshower.UI/weddingshower.UI/src/app/components/products/products-list/products-list.component.ts
+++ weddingshower.UI/weddingshower.UI/src/app/components/products/products-list/products-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
+  
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Panela',
+      image: 'string',
+      link: string;
+      reserved: boolean;
+      whoReserved: string;
+    },
+    {
 
+    },
+  ];
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+      this.products.push()
+  }
 }
---
+++ weddingshower.UI/weddingshower.UI/src/app/models/product.model.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    id: string;
+    name: string;
+    image: string;
+    link: string;
+    reserved: boolean;
+    whoReserved: string;
+}
