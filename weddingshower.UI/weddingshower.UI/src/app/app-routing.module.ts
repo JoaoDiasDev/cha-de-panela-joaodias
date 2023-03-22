c5b0e985a6c88db4be4d515dd6de6518f04442c7
JoaoDias <joaodiasworking@gmail.com>
2023-03-21 22:28:07 -0300

    First commit, initiating the project

---
+++ weddingshower.UI/weddingshower.UI/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsListComponent } from './components/products/products-list/products-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductsListComponent
+  },
+  {
+    path: 'products',
+    component: ProductsListComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
